562f9acf364af94279a92fcfd709f140e809042c
Sebastian18131 <sebastian_castellang@soy.sena.edu.co>
2025-09-22 15:51:30 -0500

    Solucionando errores

--- README.md
+++
@@ -1,2 +0,0 @@
-# Proyecto-formativo-Gestion_Documental
-Este proyecto es un sistema de gestión documental diseñado para administrar y organizar archivos de radicados de manera eficiente.
--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-// pages/_app.js
-import '@/app/globals.css'   // 👈 make sure this file exists
-import { useEffect } from "react"
-
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
-}
-
-export default MyApp
